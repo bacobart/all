39dc151472979f52311d8996b16fd254a622eeb2
Matthias Koch <ithrowexceptions@gmail.com>
2018-12-26 22:41:35 +0100

    AddProject target

--- build/Build.cs
+++ build/Build.cs
@@ -4,32 +4,34 @@ using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Git;
-using Nuke.Common.IO;
-using Nuke.Common.Tools.Git;
+using Nuke.Common.Tooling;
 using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.ControlFlow;
+using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.IO.SerializationTasks;
 using static Nuke.Common.IO.TextTasks;
+using static Nuke.Common.Logger;
 using static Nuke.Common.Tools.Git.GitTasks;
 using static TeamCityManager;
 
 partial class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.Prepare);
+    public static int Main() => Execute<Build>(x => x.CreateSolution);
 
     [Parameter] readonly bool Https;
 
     string Solution => RootDirectory / "nuke.sln";
+    string Organization => "nuke-build";
+    
     AbsolutePath RepositoriesDirectory => RootDirectory / "repositories";
-
+    AbsolutePath RepositoriesFile => RootDirectory / "repositories.yml";
     IEnumerable<GitRepository> Repositories =>
-        YamlDeserializeFromFile<string[]>(RootDirectory / "repositories.yml")
+        YamlDeserializeFromFile<string[]>(RepositoriesFile)
             .Select(x => x.Split(separator: '#'))
             .Select(x => GitRepository.FromUrl(url: x.First(), branch: x.ElementAtOrDefault(index: 1) ?? "master"));
 
-    Target Checkout => _ => _
+    Target CreateSolution => _ => _
         .Executes(() =>
         {
             foreach (var repository in Repositories)
@@ -43,7 +45,64 @@ partial class Build : NukeBuild
                 else
                     Git($"remote set-url origin {origin}", repositoryDirectory);
             }
+            
+            PrepareSolution();
         });
+
+    [Parameter(Name = "name")] readonly string PascalName;
+    [Parameter] string LispName;
+    [Parameter] readonly string[] Description;
+    [Parameter] readonly string DefaultBranch = "master";
+    [PathExecutable] readonly Tool Hub;
+    
+    Target AddProject => _ => _
+        .Requires(() => PascalName)
+        .Requires(() => Description)
+        .Executes(() =>
+        {
+            LispName = LispName ?? PascalName.SplitCamelHumpsWithSeparator("-");
+            var repositoryDirectory = RepositoriesDirectory / Organization / LispName;
+            using (SwitchWorkingDirectory(repositoryDirectory))
+            {
+                CopyTemplate(repositoryDirectory);
+                EnsureCleanDirectory(repositoryDirectory / ".git");
+
+                Git("init");
+                Git($"checkout -b {DefaultBranch}");
+                Git($"commit -m {"Initialize repository".DoubleQuote()} --allow-empty");
+                Hub($"create {Organization}/{LispName} -d {Description.JoinSpace().DoubleQuoteIfNeeded()} -h https://nuke.build");
+                Git("add .");
+                Git($"commit -m {"Add template files".DoubleQuote()}");
+            }
+
+            var updatedRepositories = Repositories
+                .Concat(GitRepository.FromUrl($"https://github.com/{Organization}/{LispName}", DefaultBranch))
+                .Select(x => $"{x.HttpsUrl}").OrderBy(x => x).ToList();
+            YamlSerializeToFile(updatedRepositories, RepositoriesFile);
+            
+            PrepareSolution();
+        });
+
+    void CopyTemplate(AbsolutePath repositoryDirectory)
+    {
+        var templateDirectory = RepositoriesDirectory / Organization / "template";
+        CopyDirectoryRecursively(templateDirectory, repositoryDirectory);
+
+        var replacements = new Dictionary<string, string>
+                           {
+                               { "Template", PascalName },
+                               { "template", LispName }
+                           };
+        FillTemplateFile(repositoryDirectory / ".nuke", replacements: replacements);
+        FillTemplateFile(repositoryDirectory / "nuke-template.sln", replacements: replacements);
+        
+        GlobDirectories(repositoryDirectory, "**/Nuke.*").ToList()
+            .ForEach(x => Directory.Move(x, x.Replace("Template", PascalName)));
+        GlobFiles(repositoryDirectory, "**/Nuke.*").ToList()
+            .ForEach(x => File.Move(x, x.Replace("Template", PascalName)));
+        GlobFiles(repositoryDirectory, "nuke-*").ToList()
+            .ForEach(x => File.Move(x, x.Replace("template", LispName)));
+    }
 
     Target Readme => _ => _
         .Executes(() =>
@@ -51,18 +110,31 @@ partial class Build : NukeBuild
             ReadmeManager.WriteReadme(RootDirectory / "README.md", RepositoriesDirectory);
         });
 
-    Target Prepare => _ => _
-        .DependsOn(Checkout)
-        .Executes(() =>
-        {
-            PrepareSolution();
-        });
-
     string TeamCityConfiguration => RootDirectory / ".teamcity" / "settings.kts";
     
-    Target TeamCity => _ => _
+    Target CreateTeamCity => _ => _
         .Executes(() =>
         {
             WriteTeamCityConfiguration(TeamCityConfiguration, Repositories.ToList());
         });
+
+    IDisposable SwitchWorkingDirectory(string workingDirectory, bool allowCreate = true)
+    {
+        if (!Directory.Exists(workingDirectory))
+            EnsureCleanDirectory(workingDirectory);
+
+        var previousWorkingDirectory = EnvironmentInfo.WorkingDirectory;
+        return DelegateDisposable.CreateBracket(
+            () => Directory.SetCurrentDirectory(workingDirectory),
+            () => Directory.SetCurrentDirectory(previousWorkingDirectory));
+    }
+
+    void FillTemplateFile(
+        string templateFile,
+        IReadOnlyCollection<string> definitions = null,
+        IReadOnlyDictionary<string, string> replacements = null)
+    {
+        var templateContent = ReadAllText(templateFile);
+        WriteAllText(templateFile, TemplateUtility.FillTemplate(templateContent, definitions, replacements));
+    }
 }
